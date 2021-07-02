df5616fbbeda2a60bfe895bde55f14fdfb01e9e7
Xiao Yu <jasonxyu@tencent.com>
2021-07-02 15:12:57 +0800

    added performance measurement scripts for PR and LPA

--- scripts/measure/measure_pagerank.sh
+++ scripts/measure/measure_pagerank.sh
@@ -6,9 +6,11 @@ cd $ROOT_DIR
 
 INCSV=${INCSV:='twitter_cont'}
 INPUT="hdfs://100.109.192.161:9000/user/plato/data/${INCSV}.csv"
-OUTPUT="hdfs://100.109.192.161:9000/user/plato/${INCSV}_lpa"
+OUTPUT="hdfs://100.109.192.161:9000/user/plato/${INCSV}_pagerank"
 
 # program config
+EPS=${EPS:=0.00001}
+DAMPING=${DAMPING:=0.85}
 ITERATIONS=1000
 NOT_ADD_REVERSED_EDGE=${NOT_ADD_REVERSED_EDGE:=true}  # let plato auto add reversed edge or not
 PART_BY_IN=false
@@ -26,15 +28,15 @@ export LD_LIBRARY_PATH=${JAVA_HOME}/jre/lib/amd64/server:${LD_LIBRARY_PATH}
 export CLASSPATH=${HADOOP_HOME}/etc/hadoop:`find ${HADOOP_HOME}/share/hadoop/ | awk '{path=path":"$0}END{print path}'`
 export LD_LIBRARY_PATH="${HADOOP_HOME}/lib/native":${LD_LIBRARY_PATH}
 
-run_lpa()
+run_pagerank()
 {
-    MAIN="$ROOT_DIR/bazel-bin/example/lpa" # process name
+    MAIN="$ROOT_DIR/bazel-bin/example/pagerank" # process name
 
     export MPIRUN_CMD=${MPIRUN_CMD:="$ROOT_DIR/3rd/mpich/bin/mpiexec.hydra"}
 
     PARAMS+=" --threads ${WCORES}"
     PARAMS+=" --input ${INPUT} --output ${OUTPUT} --is_directed=${NOT_ADD_REVERSED_EDGE}"
-    PARAMS+=" --iterations ${ITERATIONS}"
+    PARAMS+=" --iterations ${ITERATIONS} --eps ${EPS} --damping ${DAMPING}"
 
     chmod 777 ${MAIN}
     ${MPIRUN_CMD} -n ${WNUM} -hosts ${HOSTS} ${MAIN} ${PARAMS}
@@ -43,7 +45,7 @@ run_lpa()
 grep_performance()
 {
     echo $LOG_FILE
-    cat $LOG_FILE | grep "lpa calculation done"
+    cat $LOG_FILE | grep "iteration done" -A 7
 }
 
 config_hosts()
@@ -65,8 +67,8 @@ run_single()
 {
     local HOST_NUM=$1
     config_hosts $HOST_NUM
-    LOG_FILE="$ROOT_DIR/scripts/log/${INCSV}.lpa.${HOST_NUM}.${ITERATIONS}.log"
-    run_lpa > $LOG_FILE 2>&1
+    LOG_FILE="$ROOT_DIR/scripts/log/${INCSV}.pagerank.${HOST_NUM}.${ITERATIONS}.log"
+    run_pagerank > $LOG_FILE 2>&1
     grep_performance
 }
 
